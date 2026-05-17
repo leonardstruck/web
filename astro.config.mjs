a4a91d9d8572fb4e39dfa3c4ef7a7708be3cbee7
Leonard Struck <hello@leonardstruck.com>
2026-05-17 14:33:35 +0200

    Integrate TailwindCSS, update project structure, and add global styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Fira Code",
+      cssVariable: "--font-fira-code",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inconsolata",
+      cssVariable: "--font-inconsolata",
+    },
+  ],
+});
